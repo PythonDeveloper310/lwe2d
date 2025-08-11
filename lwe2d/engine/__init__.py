797f083f1ba3967ba0520d64d911913b55f150a0
amm_2000 <155813479+PythonDeveloper310@users.noreply.github.com>
2025-08-11 21:56:16 +0800

    Delete

--- lwe2d/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import Engine
-from .entity import Entity
-from pygame import Vector2
-
-__all__ = ['Engine', 'Entity', 'Vector2']
--- lwe2d/examples/engine/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import Engine
-from .entity import Entity
-from pygame import Vector2 as Vec2
-
-__all__ = ['Engine', 'Entity', 'Vec2']
